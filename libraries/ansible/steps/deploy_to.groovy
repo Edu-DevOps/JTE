0987fa88570716a5fdb377b009c65d3c9b8a8942
Edu-DevOps <77807323+Edu-DevOps@users.noreply.github.com>
2021-07-25 14:09:31 +0200

    Update deploy_to.groovy

--- libraries/ansible/steps/deploy_to.groovy
+++ libraries/ansible/steps/deploy_to.groovy
@@ -1,5 +1,5 @@
 void call(app_env){
-  stage('Deploying to ${app_env.short_name}'){
+  stage("Deploying to ${app_env.short_name}"){
     echo "Im deploying to ${app_env.eks_cluster} cluster."
   }
 }
