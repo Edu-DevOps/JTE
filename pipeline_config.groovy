04ae9ad45d9693db12de1ca019fc2b1a3b14ddbe
Edu-DevOps <77807323+Edu-DevOps@users.noreply.github.com>
2021-07-25 14:06:53 +0200

    Update pipeline_config.groovy

--- pipeline_config.groovy
+++ pipeline_config.groovy
@@ -1,7 +1,18 @@
 libraries{
+  ansible{
+    deploy_to
+  }
   maven{
     message = "My config message"
     build
-    unit_tests
+  }
+}
+
+application_environments{
+  dev{
+    eks_cluster = "eks_lab01"
+  }
+  pre{
+    eks_cluster = "eks_pre"
   }
 }
